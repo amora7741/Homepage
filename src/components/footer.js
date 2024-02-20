f37af4a29ccc349f843cfc63e6a086e6052266d2
amora7741 <mora7741@gmail.com>
2024-02-20 13:21:49 -0800

    Complete footer contact info

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,24 +17,52 @@ export default function renderFooter() {
 
   const address = document.createElement('p');
 
-  address.innerHTML = '1234 Random Road <br> Random Town, California 12345';
+  address.innerHTML = '1234 Random Road <br>Random Town, California 12345';
 
   const phone = document.createElement('div');
+  phone.className = 'contactinfocontainer';
+
+  const phoneButton = document.createElement('button');
+  phoneButton.className = 'footer icon';
 
   const phoneIcon = new Image();
   phoneIcon.src = phoneSvg;
 
-  phone.appendChild(phoneIcon);
+  phoneButton.appendChild(phoneIcon);
+
+  phone.appendChild(phoneButton);
 
   const phoneNumber = document.createElement('p');
   phoneNumber.textContent = '(555) 555-5555';
 
   phone.appendChild(phoneNumber);
 
+  const email = document.createElement('div');
+  email.className = 'contactinfocontainer';
+
+  const emailButton = document.createElement('button');
+  emailButton.className = 'footer icon';
+
+  const emailIcon = new Image();
+  emailIcon.src = emailSvg;
+
+  emailButton.appendChild(emailIcon);
+
+  email.appendChild(emailButton);
+
+  const emailAddress = document.createElement('p');
+  emailAddress.textContent = 'ashleywilliams.is.not.real@gmail.com';
+
+  email.appendChild(emailAddress);
+
+  const icons = document.createElement('div');
+  icons.className = 'icons';
+
   contactInfo.appendChild(contactMe);
   contactInfo.appendChild(getInTouch);
   contactInfo.appendChild(address);
   contactInfo.appendChild(phone);
+  contactInfo.appendChild(email);
 
   footer.appendChild(contactInfo);
 
